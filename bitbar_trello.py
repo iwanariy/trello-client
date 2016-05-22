e4233e73d9c07054702a17bd4be75d51ef684cbd
narikin <yuki.iwnr@gmail.com>
2016-05-22 20:49:53 +0900

    Add get_config()

--- bitbar_trello.py
+++ bitbar_trello.py
@@ -2,5 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
+CONFIG = "./config.ini"
+
+import configparser
+import sys
+
+
+def get_config(path):
+    """ Get config object """
+    config = configparser.ConfigParser()
+
+    try:
+        config.read(path)
+        return config
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
 if __name__ == u"__main__":
     pass
