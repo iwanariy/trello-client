f61dbc2d24b914535974383a46ff3551e18bc971
narikin <yuki.iwnr@gmail.com>
2016-05-26 22:12:10 +0900

    Create board.py

---
+++ trello/board.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Board(object):
+    """
+    Class Board in trello
+    """
+
+    def __init__(self, board_id):
+        self.id = board_id
