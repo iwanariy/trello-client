f2a7027d18e4d0c0aa81d070200be7dc5db2fcbb
narikin <yuki.iwnr@gmail.com>
2016-05-26 23:40:23 +0900

    Create trello_list.py

---
+++ trello/trello_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class List(object):
+    """
+    Class List in trello
+    """
+
+    def __init__(self, list_id, client):
+        self.id = list_id
+        self.client = client
+
+    def fetch(self):
+        """ Fetch all attributes """
+        json_obj = self.client.get_json("/lists/" + self.id)
+
+        self.name = json_obj["name"]
