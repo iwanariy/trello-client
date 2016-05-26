1b0af93e48c752cf116681019fb2875ad590ce7b
narikin <yuki.iwnr@gmail.com>
2016-05-26 23:20:28 +0900

    Add fetch() of Board class

--- trello/board.py
+++ trello/board.py
@@ -7,5 +7,12 @@ class Board(object):
     Class Board in trello
     """
 
-    def __init__(self, board_id):
+    def __init__(self, board_id, client):
         self.id = board_id
+        self.client = client
+
+    def fetch(self):
+        """ Fetch all attributes """
+        json_obj = self.client.get_json("/boards/" + self.id)
+
+        self.name = json_obj["name"]
