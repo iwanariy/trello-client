533a746e421cabfb15f21be35e88620329b595d1
narikin <yuki.iwnr@gmail.com>
2016-05-28 15:12:44 +0900

    Add Card class

---
+++ trello/card.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Card(object):
+    """
+    Card class in trello
+    """
+
+    def __init__(self, client, card_id, name=""):
+        self.id = card_id
+
+        self.client = client
+        self.name = name
+
+    def fetch(self):
+        """ Fetch all attributes """
+        json_obj = self.client.get_json("/cards/" + self.id)
+
+        self.name = json_obj["name"]
