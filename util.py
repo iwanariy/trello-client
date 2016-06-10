c5484becf4a5adbb00dec93022f10ed0fc73e0ed
narikin <yuki.iwnr@gmail.com>
2016-06-10 15:35:44 +0900

    move get_crediential()

--- util.py
+++ util.py
@@ -2,5 +2,21 @@
 # -*- coding: utf-8 -*-
 
 
+from trello import get_config
+
+CONFIG = "./config.cfg"
+
+
+def get_credential():
+    """ Get crediential for trello """
+    config = get_config(CONFIG)
+
+    username = config.get("trello", "username")
+    key = config.get("trello", "key")
+    token = config.get("trello", "token")
+
+    return username, key, token
+
+
 if __name__ == u"__main__":
     pass
